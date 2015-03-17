dee0bece5b953f94513a17a013691e9e5b748691
Stephen <spow@NTCH-D0072-ubuntu.(none)>
2015-03-17 19:40:29 +0100

    Further tidying

--- code/test.c
+++ code/test.c
@@ -16,75 +16,87 @@
 
 main(int argc, char *argv[])
 {
-	int i, j,k,l;
+  
+	//counters
+	  int i, j,k,l;
 	
-	double realE=0.0;
-	double radius=2.0;
-	int Ntot, Ntot2, Ntot3, Nrem, Nrem3, Ntemp, Ntemp2, length1=6, length1a=length1, length2 = length1*3 , geo=0;
-	int botcells=0, topcells=0, midcells=1, procs=1, this_proc=0, remainder, Epts_temp=11, en, numfin, num_rows=10, Epoints=11, num_hole_rows;
- 	double Emax=-0.9, Emin=0.9, Estep=0.04, Emin_temp=0.0, pos_err=0.0, rad_err=0.0, rad_dis=0.0, dis_pot=0.0, anglein, angle;
-	double _Complex En;
-	int conf_num; 
-	char job_name[64];
-	double suba_conc, suba_pot, subb_conc, subb_pot;
-	int output_type=1;   //=0 no structure output, =1 just atoms by sublattice, =2 atoms and connectors
-	int buffer_rows, mappings;		   
-			      	    double dos, cond, cond2;
-
+	//length and index array related constants
+	  int Ntot, Nrem, length1=6,  length2 = length1*3 , geo=0;
 	
-      if(argc ==17)
-      {
-	      sscanf(argv[1], "%s", job_name);
-	      sscanf(argv[2], "%d", &geo);
-	      sscanf(argv[3], "%d", &length1);
-	      sscanf(argv[4], "%d", &length2);
-	      sscanf(argv[5], "%d", &buffer_rows);
-	      
-	      sscanf(argv[6], "%lf", &suba_conc);
-	      sscanf(argv[7], "%lf", &suba_pot);
-	      
-	      sscanf(argv[8], "%lf", &subb_conc);
-	      sscanf(argv[9], "%lf", &subb_pot);
-	      
-	      sscanf(argv[10], "%lf", &Emin);
-	      sscanf(argv[11], "%lf", &Emax);
-	      sscanf(argv[12], "%d", &Epoints);
-	      sscanf(argv[13], "%d", &mappings); 	//how frequently to make maps (0 = never, 1 = always, 2 = every second energy etc, -n every nth energy but only for conf 0)
+	//processor and energy loop related integers
+	  int procs=1, this_proc=0, remainder, Epts_temp=11, en, numfin, Epoints=11, conf_num=0;
+	  
+	//energy loop related doubles 
+	  double realE=0.0, Emax=-0.9, Emin=0.9, Estep=0.04, Emin_temp=0.0;
+	  double _Complex En;
+	  
+	//disorder / system config parameters
+	  double suba_conc, suba_pot, subb_conc, subb_pot;
 
-	      sscanf(argv[14], "%d", &conf_num);
-	      sscanf(argv[15], "%d", &procs);
-	      sscanf(argv[16], "%d", &this_proc);
-      }
-      
-      else
-      {
-	      exit(1);
-      }
+	//misc  
+	  char job_name[64];
+	  int output_type=1;   //=0 no structure output, =1 just atoms by sublattice, =2 atoms and connectors
+	  int buffer_rows, mappings;		   
+	  double cond2;
+
+	  
+	  
+	//Read command line input - quit if incorrect input
+	  //Maybe replace this with input file?
+	    if(argc ==17)
+	    {
+		    sscanf(argv[1], "%s", job_name);
+		    sscanf(argv[2], "%d", &geo);
+		    sscanf(argv[3], "%d", &length1);
+		    sscanf(argv[4], "%d", &length2);
+		    sscanf(argv[5], "%d", &buffer_rows);
+		    
+		    sscanf(argv[6], "%lf", &suba_conc);
+		    sscanf(argv[7], "%lf", &suba_pot);
+		    
+		    sscanf(argv[8], "%lf", &subb_conc);
+		    sscanf(argv[9], "%lf", &subb_pot);
+		    
+		    sscanf(argv[10], "%lf", &Emin);
+		    sscanf(argv[11], "%lf", &Emax);
+		    sscanf(argv[12], "%d", &Epoints);
+		    sscanf(argv[13], "%d", &mappings); 	//how frequently to make maps (0 = never, 1 = always, 
+								//2 = every second energy etc, -n every nth energy but only for conf 0)
+
+		    sscanf(argv[14], "%d", &conf_num);
+		    sscanf(argv[15], "%d", &procs);
+		    sscanf(argv[16], "%d", &this_proc);
+	    }
+	    
+	    else
+	    {
+		    exit(1);
+	    }
       
 
+	//File I/O variables
+	    FILE *output;
+	    char filename[160], filename3[160], filename_temp[128], checkname[128], direcname[128], conffile[128], strucfile[128];
+            char command[256];
+	    
+	//Create directory and filenaming convention
+	    sprintf(direcname, "res/%s", job_name);
+	    sprintf(command, "mkdir -p %s", direcname);
+	    system(command);
+	    
+	    sprintf(filename_temp, "%s.conf%02d", job_name, conf_num); 
+	    sprintf(strucfile, "%s/%s.struct", direcname, filename_temp);
+	    sprintf(filename, "%s/.%s.part%02d", direcname, filename_temp, this_proc);
 	
-      FILE *output, *output2;
-     char filename[160], filename2[160], filename3[160], filename_temp[128], checkname[128], finalname[128], direcname[128], conffile[128], strucfile[128];
-      
-      char command[256];
-      sprintf(direcname, "res/%s", job_name);
-      sprintf(command, "mkdir -p %s", direcname);
-      system(command);
-      
-
-
-      sprintf(filename_temp, "%s.conf%02d", job_name, conf_num); 
-      sprintf(strucfile, "%s/%s.struct", direcname, filename_temp);
-
-      sprintf(filename, "%s/.%s.part%02d", direcname, filename_temp, this_proc);
-      output =fopen(filename, "w");
-      fclose(output);
+	//Create main output file
+	    output =fopen(filename, "w");
+	    fclose(output);
 
       
       
-      //status files for multiprocessor runs
+	//Status files for multiprocessor runs
       
-      	      //Check file - this keeps track of how many of the processes are complete
+	    //Check file - this keeps track of how many of the processes are complete
 		FILE *check;
 		sprintf(checkname, "%s/.%s.CHECK", direcname, filename_temp);
 
@@ -112,12 +124,7 @@ main(int argc, char *argv[])
 
  	      }
       
-      
-      int number, number2;
-      int rows =1;
-      int numholes, numholes2, numholes_clean;
-           
-      double offset= sqrt(3) *length1a /4;
+
 
     
       
@@ -176,40 +183,40 @@ main(int argc, char *argv[])
 		      realE =  Emin_temp + en*Estep;
 		      sprintf(filename3, "%s/%s.en_%.3lf", direcname, filename_temp, realE);
 
-// 		      if(mappings == 0)
-// 		      {
-// 			cond2 = genConduc5(realE + eta*I, &System, -1.0);
-// 		      }
-// 		      
-// 		      if(mappings > 0)
-// 		      {
-// 			if((en % mappings) == 0)
-// 			{
-// 			  cond2 = genConduc4(realE + eta*I, &System, ldoses, conds, indices, neigh, -1.0);
-// 			  CurrentMapOut(&System, conds, indices, neigh, filename3);
-// 			  LDOSMapOut(&System, ldoses, filename3);
-// 			}
-// 			if((en % mappings) != 0)
-// 			{
-// 			  cond2 = genConduc5(realE + eta*I, &System, -1.0);
-// 			}
-// 		      }
-// 		      
-// 		      if(mappings < 0)
-// 		      {
-// 			if((en % mappings) == 0 && conf_num == 0)
-// 			{
-// 			  cond2 = genConduc4(realE + eta*I, &System, ldoses, conds, indices, neigh, -1.0);
-// 			  CurrentMapOut(&System, conds, indices, neigh, filename3);
-// 			  LDOSMapOut(&System, ldoses, filename3);
-// 			}
-// 			if((en % mappings) != 0 || conf_num != 0)
-// 			{
-// 			  cond2 = genConduc5(realE + eta*I, &System, -1.0);
-// 			}
-// 		      }
+		      if(mappings == 0)
+		      {
+			cond2 = genConduc5(realE + eta*I, &System, -1.0);
+		      }
 		      
-		      printf("%lf	%e\n", realE,  genConduc5(realE + eta*I, &System, -1.0));
+		      if(mappings > 0)
+		      {
+			if((en % mappings) == 0)
+			{
+			  cond2 = genConduc4(realE + eta*I, &System, ldoses, conds, indices, neigh, -1.0);
+			  CurrentMapOut(&System, conds, indices, neigh, filename3);
+			  LDOSMapOut(&System, ldoses, filename3);
+			}
+			if((en % mappings) != 0)
+			{
+			  cond2 = genConduc5(realE + eta*I, &System, -1.0);
+			}
+		      }
+		      
+		      if(mappings < 0)
+		      {
+			if((en % mappings) == 0 && conf_num == 0)
+			{
+			  cond2 = genConduc4(realE + eta*I, &System, ldoses, conds, indices, neigh, -1.0);
+			  CurrentMapOut(&System, conds, indices, neigh, filename3);
+			  LDOSMapOut(&System, ldoses, filename3);
+			}
+			if((en % mappings) != 0 || conf_num != 0)
+			{
+			  cond2 = genConduc5(realE + eta*I, &System, -1.0);
+			}
+		      }
+		      
+		  //    printf("%lf	%e\n", realE,  genConduc5(realE + eta*I, &System, -1.0));
 			
 		      //cond2 =genConduc5(realE + eta*I, &System, -1.0);
 		      
